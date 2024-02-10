136a87a8dee92f53515fcef7907b9254da253459
student <iseexam43@vvce.ac.in>
2024-02-10 12:41:47 +0530

    commit

---
+++ swap.java
@@ -0,0 +1,11 @@
+class swap
+{
+public static void main(String[] args){
+int x=10,y=12,t;
+System.out.println("Before swapping" +x+" "+y);
+t=x;
+x=y;
+y=t;
+System.out.println("After swapping" +x+" "+y);
+}
+}
